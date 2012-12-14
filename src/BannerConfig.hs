3b965081bbefddb88299697c9bde29d9b8de6ee2
David Shere <david.e.shere@gmail.com>
2012-12-13 18:53:54 -0800

    Version 1.0 -- Everything seems to work as expected.

--- src/BannerConfig.hs
+++ src/BannerConfig.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module BannerConfig (BannerConfig, StatConfig, Color, Position, getBannerConfig) where
+module BannerConfig (
+    BannerConfig(BannerConfig, backgroundImage, statConfigs),
+    StatConfig(StatConfig),
+    Color(Color),
+    Position(Position),
+    getBannerConfig) where
 
 import Data.Yaml.YamlLight
 import qualified Data.ByteString.Char8 as BS
@@ -9,16 +14,16 @@ import Data.Maybe
 import qualified Data.Map as M
 
 data BannerConfig = BannerConfig
-    { id          :: String
-    , queryURL    :: String
-    , bgImage     :: String
-    , statConfigs :: [StatConfig]
+    { queryURL        :: String
+    , backgroundImage :: String
+    , statConfigs     :: [StatConfig]
     } deriving (Show)
 
 data StatConfig = StatConfig
     { key      :: String
     , name     :: String
-    , size     :: Int
+    , fontFace :: String
+    , size     :: Double
     , color    :: Color
     , position :: Position
     } deriving (Show)
@@ -31,8 +36,8 @@ data Color = Color
     } deriving (Show)
 
 data Position = Position
-    { x :: Int
-    , y :: Int
+    { x :: Double
+    , y :: Double
     } deriving (Show)
 
 getBannerConfig :: YamlLight -> Maybe BannerConfig
@@ -41,9 +46,8 @@ getBannerConfig map = unMap map >>= makeBannerConfig
 makeBannerConfig :: Map YamlLight YamlLight -> Maybe BannerConfig
 makeBannerConfig map = Just
     (BannerConfig {
-        BannerConfig.id = getId map,
         queryURL        = getQueryURL map,
-        bgImage         = getBGImage map,
+        backgroundImage = getBGImage map,
         statConfigs     = getStatConfigs map
       }
     )
@@ -55,9 +59,8 @@ getString key map = case lookup of
   where
     lookup = M.lookup (YStr key) map >>= unStr >>= (\bs -> (Just (BS.unpack bs)))
 
-getId          = getString "id"
 getQueryURL    = getString "queryURL"
-getBGImage     = getString "bgImage"
+getBGImage     = getString "backgroundImage"
 
 getStatConfigs :: Map YamlLight YamlLight -> [StatConfig]
 getStatConfigs map = case lookup of 
@@ -69,7 +72,8 @@ getStatConfigs map = case lookup of
     getStatConfig map = StatConfig {
              key  = getString "key" (fromJust map),
              name = getString "name" (fromJust map),
-             size = getInt "size" (fromJust map),
+             fontFace = getString "font" (fromJust map),
+             size = getDouble "size" (fromJust map),
              color = getColor (fromJust map),
              position = getPosition (fromJust map)}
 
@@ -92,12 +96,9 @@ getPosition map = case lookup of
   where
     lookup           = M.lookup (YStr "position") map >>= unMap >>= makePosition
     makePosition map = Just (Position
-                            (getInt "x" map)
-                            (getInt "y" map))
+                            (getDouble "x" map)
+                            (getDouble "y" map))
 
 
 getDouble :: BS.ByteString -> Map YamlLight YamlLight -> Double
 getDouble key map = read $ getString key map :: Double
-
-getInt :: BS.ByteString -> Map YamlLight YamlLight -> Int
-getInt key map = read $ getString key map :: Int
--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,34 @@
 {-# LANGUAGE DeriveDataTypeable, RecordWildCards #-}
 
+-- This program creates a statistics image banner for a user or team
+-- involved in Folding@home.  I used the singleMode.hs example from 
+-- http://zuttobenkyou.wordpress.com/2011/04/19/haskell-using-cmdargs-single-and-multi-mode/
+-- to make the commandline options for this program look pretty.
+
 import System.Console.CmdArgs
 import System.Environment (getArgs, withArgs)
 import System.Exit
 import Control.Monad (when)
-
+import Network.Download
+import Data.Maybe
 import Data.Yaml.YamlLight
-
+import Text.XML.Light
+import Text.XML.Light.Cursor
+import Graphics.Rendering.Cairo
 import BannerConfig
 
 data MyOptions = MyOptions
     { config :: FilePath
+    , output :: FilePath
+    , ident  :: String
     } deriving (Data, Typeable, Show, Eq)
 
 -- Customize your options, including help messages, shortened names, etc.
 myProgOpts :: MyOptions
 myProgOpts = MyOptions
-    {config = def &= typFile &= help "the banner configuration file" }
+    { config = def &= typFile &= help "the banner configuration file"
+    , output = def &= typFile &= help "the output file (must be png)"
+    , ident  = def &= typ "ID" &= help "the user or team id" }
 
 getOpts :: IO MyOptions
 getOpts = cmdArgs $ myProgOpts
@@ -28,7 +40,7 @@ getOpts = cmdArgs $ myProgOpts
     &= program _PROGRAM_NAME
 
 _PROGRAM_NAME = "foldbanner"
-_PROGRAM_VERSION = "0.1"
+_PROGRAM_VERSION = "1.0"
 _PROGRAM_INFO = _PROGRAM_NAME ++ " version " ++ _PROGRAM_VERSION
 _PROGRAM_ABOUT = "A configurable program for generating statistics banners for Folding@home"
 _COPYRIGHT = "(C) David Shere 2012"
@@ -45,10 +57,55 @@ optionHandler :: MyOptions -> IO ()
 optionHandler opts@MyOptions{..}  = do
     -- Take the opportunity here to weed out ugly, malformed, or invalid arguments.
     when (null config) $ putStrLn "--config is blank!" >> exitWith (ExitFailure 1)
+    when (null output) $ putStrLn "--output is blank!" >> exitWith (ExitFailure 1)
+    when (null ident)  $ putStrLn "--ident is blank!"  >> exitWith (ExitFailure 1)
     -- When you're done, pass the (corrected, or not) options to your actual program.
     exec opts
 
 exec :: MyOptions -> IO ()
 exec opts@MyOptions{..} = do
-    yamlConfig <- parseYamlFile config
-    (print . getBannerConfig) yamlConfig
+    c <- parseYamlFile config
+    let cfg = fromJust $ getBannerConfig c
+    stats <- getStats cfg ident
+    createBanner cfg (fromJust stats) output
+
+-------------------------------------------------------------------------------
+
+getStats :: BannerConfig -> String -> IO (Maybe Element)
+getStats (BannerConfig url _ _) id = do
+    xml <- openAsXML $ url ++ id
+    case xml of
+        Left _      -> return Nothing
+        Right stats -> return $ Just ((onlyElems stats) !! 1)
+
+createBanner :: BannerConfig -> Element -> FilePath -> IO ()
+createBanner cfg stats output = withImageSurfaceFromPNG (backgroundImage cfg) $ \surface -> do
+    renderWith surface $ do mapM_ writeStat $ (statConfigs cfg)
+    surfaceWriteToPNG surface output
+    return ()
+  where
+    writeStat (StatConfig key name fontFace size color pos) = do
+        selectFontFace fontFace FontSlantNormal FontWeightNormal
+        setFontSize size
+        writeText (name ++ ": " ++ (getData key)) color pos
+    getData key = case findElementByName key stats of
+        Nothing -> "Error"
+        Just e -> strContent e
+    findElementByName key stats = findElement (unqual key) stats
+
+writeText :: String -> Color -> Position -> Render ()
+writeText str (Color r g b a) (Position x y) = do
+    save
+
+    lineWidth <- getLineWidth
+
+    (TextExtents xb yb w h _ _) <- textExtents str
+
+    moveTo x y
+    textPath str
+    setSourceRGBA r g b a
+    fillPreserve
+    setLineWidth 0.75
+    stroke
+
+    restore
